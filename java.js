666bd5a37bb43cb42a07345f54685018a6e00362
JagdishPandhare <jagdishpandhare678@gmail.com>
2025-01-21 09:28:58 +0530

    Initial commit

---
+++ java.js
@@ -0,0 +1,12 @@
+// This is an empty JavaScript file for your portfolio project.
+// You can start adding your JavaScript code here.
+
+console.log("Hello, world!");
+
+// Example function to display a greeting message
+function greet(name) {
+    return `Hello, ${name}! Welcome to my portfolio.`;
+}
+
+// Example usage of the greet function
+console.log(greet("Visitor"));
